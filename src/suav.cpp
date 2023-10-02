ffb5e939148458cb398acad17515a213fb3a39de
xirhxqOnAmov <373836275@qq.com>
2023-10-02 15:36:33 +0800

    add flag receiving

--- src/suav.cpp
+++ src/suav.cpp
@@ -130,6 +130,10 @@ public:
         task_state = LAND;
     }
 
+    void toStepBack(){
+        task_state = BACK;
+    }
+
     void toStepEnd() {
         task_state = END;
     }
@@ -157,8 +161,18 @@ public:
         // fc.M210_adjust_yaw(fc.yaw_offset);
         fc.UAV_Control_to_Point_with_yaw(expected_point, fc.yaw_offset);
         if (fc.enough_time_after(hold_begin_time, hold_time) && searchOver){
-            toStepLand();
+            toStepBack();
         }
+    }
+
+    void StepBack() {
+        ROS_INFO("###----StepBack----###");
+        double hold_time = 5.0;
+        auto expected_point = MyDataFun::new_point(-1.5, -1.5, 2.0);
+        ROS_INFO("Back %.2lf", fc.get_time_now() - hold_begin_time);
+        ROS_INFO("ExpectedPoint: %s", MyDataFun::output_str(expected_point).c_str());
+        ROS_INFO("Search over: %s", searchOver?"YES":"NO");
+        fc.UAV_Control_to_Point_with_yaw(expected_point, fc.yaw_offset);
     }
 
     void StepLand() {
@@ -186,6 +200,10 @@ public:
             }
             case LAND: {
                 StepLand();
+                break;
+            }
+            case BACK: {
+                StepBack();
                 break;
             }
             default: {
